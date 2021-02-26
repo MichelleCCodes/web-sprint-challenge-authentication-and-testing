366ae2d224b10d978d6fac22cf2a29044fc4b940
Michelle Chen <mchen0062@gmail.com>
2021-02-26 08:59:05 -0800

    created secret and validate user info exists

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const cors = require('cors');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const restrict = require('./middleware/restricted.js');
 
@@ -10,10 +10,15 @@ const jokesRouter = require('./jokes/jokes-router.js');
 const server = express();
 
 server.use(helmet());
-server.use(cors());
 server.use(express.json());
+server.use(cors());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
 
+server.get("/", (req, res)=> {
+    res.json("Hello fromSprint Challenge 3!")
+})
 module.exports = server;
+
+//should middleware be imported into router file instead? 
